0927939a2d979deb69f28a36ef00935b92c11473
Francois Wolfaardt <francoisw812@gmail.com>
2023-09-12 09:41:00 +0200

    updated profile video page

--- src/views/CreateProfileVideo/index.tsx
+++ src/views/CreateProfileVideo/index.tsx
@@ -1,23 +1,22 @@
 import React, { FC, useEffect, useState, useRef } from "react";
+
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
-import { RNCamera } from 'react-native-camera';
+
 import moment from 'moment';
-
-import { View, Image, ScrollView } from "react-native";
-import { useTheme, TextInput, Button, Text, Snackbar } from 'react-native-paper';
-import { DatePickerModal } from 'react-native-paper-dates';
+import { RNCamera } from 'react-native-camera';
 import Dropdown from 'react-native-input-select';
+import { DatePickerModal } from 'react-native-paper-dates';
 import { Container, Row, Col } from 'react-native-flex-grid';
-
-
+import { useTheme, IconButton, Button, Text, Snackbar } from 'react-native-paper';
+import { View, Image, Alert, Modal, TouchableOpacity, ImageBackground } from "react-native";
 
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { User } from '../../types';
-import { fetchJobRoles, fetchPreviousExperiences, fetchPreferredHours } from "../../actions/jobs.actions";
 import { createProfile, fetchGenders } from "../../actions/account.actions";
+import { fetchJobRoles, fetchPreviousExperiences, fetchPreferredHours } from "../../actions/jobs.actions";
 
 import { styles } from "../../theme/styles";
 import { setUser } from "../../redux/reducers/user.reducer";
@@ -25,16 +24,48 @@ import { setUser } from "../../redux/reducers/user.reducer";
 const CreateProfileVideo: FC = () => {
 
     const theme = useTheme();
-    let cameraRef = useRef(null);
+    let cameraRef = useRef<any>();
     const dispatch = useDispatch();
     const user_state = useSelector((state: RootState) => state.userSlice.user);
     const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
+
+    const [seconds, setSeconds] = useState<number>(15);
     const [user, setUserData] = useState<User>(user_state);
+    const [recording, setRecording] = useState<boolean>(false);
+    const [recording_ended, setRecordingEnded] = useState<boolean>(false);
     const [submitting, setSubmitting] = useState<boolean>(false);
+    const [modalVisible, setModalVisible] = useState<boolean>(false);
 
     const handleCreateProfile = async () => {
     }
+
+    const startRecordingVideo = async () => {
+        if (cameraRef && cameraRef.current) {
+            let camera = cameraRef.current;
+            setRecording(true);
+        
+            const options = { 
+                quality: RNCamera.Constants.VideoQuality["288p"], 
+                maxDuration: seconds 
+            };
+
+            const { uri } = await camera.recordAsync(options);
+            console.log('uri', uri);
+            Alert.alert('Video', uri);
+        }   
+    }
+
+    const stopRecordingVideo = async () => {
+        if (!recording) { return }
+        if (cameraRef && cameraRef.current) {
+            let camera = cameraRef.current;
+            setSeconds(15);
+            setRecording(false);
+            await camera.stopRecording();
+        }
+    }
+
 
     return (
         <View style={[styles.wrapper, { backgroundColor: theme.colors.primary }]}>
@@ -42,26 +73,79 @@ const CreateProfileVideo: FC = () => {
                 <Image source={require(`../../assets/images/logo_white.png`)} style={styles.xxs_image} />
             </View>
             <View style={[styles.container_curved, { backgroundColor: theme.colors.onPrimary }]}>
-                <Text style={[styles.text_light_blue_heading, { paddingTop: 10 }]} variant="headlineSmall">Record a short video intro (max 15sec)</Text>
+                <Text style={[styles.text_light_blue_heading, {paddingTop: 10, paddingBottom: 0 }]} variant="headlineSmall">Record a short video intro</Text>
+                <Text style={[styles.text_light_blue_heading]} variant="labelLarge">(max 15sec)</Text>
                 <View style={[styles.container_curved, { backgroundColor: theme.colors.primary, width: '100%', paddingTop: 0 }]}>
+                    <ImageBackground source={require(`../../assets/images/recording_intro.jpg`)} style={[styles.background_image, { width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }]}>
+                        <Button style={{ position: 'absolute', bottom: 20 }} icon="camera" mode="contained" onPress={() => setModalVisible(true)}>
+                            Start Recording
+                        </Button>
+                    </ImageBackground>
+                </View>
+            </View>
+
+            <Modal
+                style={styles.wrapper}
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+                onRequestClose={() => { setModalVisible(false)}}
+                presentationStyle={"pageSheet"}
+            >
+                <>
+                    <TouchableOpacity style={styles.modal_btn_left} onPress={() => setModalVisible(false)} >
+                        <Text style={styles.modal_txt}>Cancel</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.modal_btn_right} onPress={() => console.log()} >
+                        <Text style={styles.modal_txt}>Save</Text>
+                    </TouchableOpacity>
+
                     <RNCamera
                         ref={cameraRef}
                         style={[styles.container_curved, { flex: 1, justifyContent: 'center', alignItems: 'center', position: 'relative', backgroundColor: theme.colors.primary, width: '100%' }]}
                         type={RNCamera.Constants.Type.front}
+                        ratio={'16:9'}
                         captureAudio={true}
+                        onRecordingEnd={() => setRecordingEnded(true)}
                         androidCameraPermissionOptions={{
                             title: 'Permission to use camera',
                             message: 'We need your permission to use your camera',
                             buttonPositive: 'Ok',
                             buttonNegative: 'Cancel',
                         }}
+                        androidRecordAudioPermissionOptions={{
+                            title: 'Permission to use audio recording',
+                            message: 'We need your permission to use your audio',
+                            buttonPositive: 'Ok',
+                            buttonNegative: 'Cancel',
+                        }}
                     >
-                        <Button style={{position: 'absolute', bottom: 20}} icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-                            Start Recording
-                        </Button>
+                        {!recording &&
+                            <IconButton
+                                style={{ position: 'absolute', bottom: 20 }}
+                                icon="record-circle"
+                                iconColor={theme.colors.surface}
+                                size={70}
+                                onPress={() => startRecordingVideo()}
+                            />
+                        }
+
+                        {recording &&
+                            <>
+                            <Text style={{ position: 'absolute', top: 20 }} variant="headlineSmall">{seconds} sec left</Text>
+                            <IconButton
+                                style={{ position: 'absolute', bottom: 20 }}
+                                icon="stop-circle"
+                                iconColor={theme.colors.surface}
+                                size={70}
+                                onPress={() => stopRecordingVideo()}
+                            />
+                            </>
+                        }
                     </RNCamera>
-                </View>
-            </View>
+
+                </>
+            </Modal>
         </View>
     );
 };
